e3171daa5fb90dfb9cc248b4eb5d1d62b6ef5bb6
Yunus Emre Başer <yunusemrebaser6021@gmail.com>
2022-08-16 22:30:13 +0300

    Proje dosyası ekle.

---
+++ Extention-Recursive-Metotlar/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention_Recursive_Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursive özyinelemeli metotlar
+            //3^4 = 3*3*3*3
+
+
+            int result = 1;
+            for( int i = 1; i < 5; i++)
+            {
+                result = result * 3;
+            }
+            Console.WriteLine(result);
+
+            Console.WriteLine("***************************************************");
+
+            Islemler ınstance = new Islemler();
+            int sonuc = ınstance.Expo(3, 4);
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("******************************************************");
+
+            //Extention Metotlar
+            string ifade = "Yunus Emre Başer";
+            bool sonuc2 = ifade.CheckSpaces();
+            Console.WriteLine(sonuc2);
+            if (sonuc2)
+            {
+                Console.WriteLine(ifade.RemoveWhiteSpaces());
+            }
+
+            Console.WriteLine("*****************************************************");
+
+            Console.WriteLine(ifade.MakeUpperCase());
+            Console.WriteLine(ifade.MakeLowerCase());
+
+            Console.WriteLine("*****************************************************");
+
+            int[] dizi = { 1, 5, 8, 6, 7, 9, 2 };
+            dizi.SortedArray();
+            dizi.EkranaYazdir();
+
+            Console.WriteLine("*****************************************************");
+
+            int sayi = 55;
+            Console.WriteLine(sayi.isEvenNumber());
+
+            Console.WriteLine("******************************************************");
+
+            Console.WriteLine(ifade.GetFirstCharacter());
+
+
+
+
+        }
+    }
+
+    class Islemler
+    {
+        public int Expo(int sayi, int üs)
+        {
+            if (üs < 2)
+                return sayi;
+            return Expo(sayi, üs - 1) * sayi;
+
+            //Expo(3,4)
+            //Expo(3,3) * 3
+            //Expo(3,2) * 3 * 3
+            //Expo(3,1) * 3 * 3 *3
+            //3 * 3 * 3 * 3 = 3^4 = 81
+        }
+    }
+
+    public static class Extention
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] dizi = param.Split(' ');
+            return string.Join("", dizi);
+
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static string MakeLowerCase(this string param)
+        {
+            return param.ToLower();
+        }
+
+        public static int[] SortedArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (int item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool isEvenNumber(this int param)
+        {
+            return param % 2 == 0;
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0, 1);
+        }
+    }
+}
---
+++ Extention-Recursive-Metotlar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Extention-Recursive-Metotlar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Extention-Recursive-Metotlar")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("bbab439d-e0c3-4cb6-afe3-8ad8c616166a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
